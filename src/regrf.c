560b41ec428275ea84706a3a1697055c9ebbbf5f
Yenny Zhang <yennyszhang@gmail.com>
2015-07-08 15:07:26 -0700

    taking steps to do nothing...
    lol

--- src/regrf.c
+++ src/regrf.c
@@ -187,6 +187,11 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
         probs[k] = 1 / *sampsize;
       }
 
+      int fakecoeffs[*sampsize];
+      for (k = 0; k < *sampsize; ++k) {
+        fakecoeffs = 1;
+      }
+
       ran_multinomial(*sampsize, 100, probs, coeffs);
 
     /* be done with the multinomial */
@@ -227,7 +232,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
 		regTree(xb, yb, mdim, *sampsize, lDaughter + idx, rDaughter + idx,
                 upper + idx, avnode + idx, nodestatus + idx, *nrnodes,
                 treeSize + j, *nthsize, *mtry, mbest + idx, cat, tgini,
-                varUsed, coeffs);
+                varUsed, fakecoeffs);
         /* predict the OOB data with the current tree */
 		/* ytr is the prediction on OOB data by the current tree */
 		predictRegTree(x, nsample, mdim, lDaughter + idx,

11e070ca1c02ae4fe261d81dd6fa1e889461fcfe
Yenny Zhang <yennyszhang@gmail.com>
2015-07-08 10:52:54 -0700

    added the multinomial to the header.
    hey

--- src/rf.h
+++ src/rf.h
@@ -35,6 +35,9 @@ void classRF(double *x, int *dimx, int *cl, int *ncl, int *cat, int *maxcat,
 void normClassWt(int *cl, const int nsample, const int nclass, 
                  const int useWt, double *classwt, int *classFreq);
 
+void ran_multinomial (const size_t K, const unsigned int N, 
+                      const double p[], unsigned int n[]);
+
 void classForest(int *mdim, int *ntest, int *nclass, int *maxcat, 
                  int *nrnodes, int *jbt, double *xts, double *xbestsplit, 
                  double *pid, double *cutoff, double *countts, int *treemap, 

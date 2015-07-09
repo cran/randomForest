39b19968f2eedbb48be9eeee23f60c9bffbef01b
Yenny Zhang <yennyszhang@gmail.com>
2015-07-09 15:14:38 -0700

    testign pk

--- src/regrf.c
+++ src/regrf.c
@@ -49,7 +49,7 @@ void ran_multinomial (int K, int N,
     if (probs[k] > 0.0) 
     {   
       /*coeffs[k] = 1; */
-      coeffs[k] = rbinom(1.0 , 1.0);
+      coeffs[k] = rbinom(1.0 , p[k]);
       /*coeffs[k] = rbinom(N - sum_n , p[k] / (norm - sum_p));*/
       /*coeffs[k] = rbinom( 1.0, 1.0);*/
     }

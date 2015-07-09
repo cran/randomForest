805c6c01655f67faeb65511524dc6430d4914931
Yenny Zhang <yennyszhang@gmail.com>
2015-07-09 16:29:41 -0700

    nsample?

--- src/regrf.c
+++ src/regrf.c
@@ -199,9 +199,9 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
           /*
       double probs[*sampsize];
           */
-      for (k = 0; k < *sampsize; ++k) {
+      for (k = 0; k < nsample; ++k) {
         /*probs[k] = 1.0 / *sampsize;*/
-        probs[k] = 1.0 / *sampsize;
+        probs[k] = 1.0 / nsample;
       }
 
       /*

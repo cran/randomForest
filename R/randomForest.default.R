b38f6d80e79dc73f85a340fab621a8f46f7cdf2c
Yenny Zhang <yennyszhang@gmail.com>
2015-07-10 13:52:42 -0700

    hey

--- R/randomForest.default.R
+++ R/randomForest.default.R
@@ -407,7 +407,7 @@ mylevels <- function(x) if (is.factor(x)) levels(x) else 0
                     inbag = if (keep.inbag)
                     matrix(integer(n * ntree), n) else integer(1),
                     DUP=FALSE,
-                    coeffs = matrix(sampsize, ncol = integer(ntree)),
+                    coeffs = matrix(integer(ntree), ncol = sampsize),
                     PACKAGE="randomForest")[c(16:28, 36:41)]
         ## Format the forest component, if present.
         if (keep.forest) {

0a4fd5f85b26232748f72c3f60b5739722bed584
Yenny Zhang <yennyszhang@gmail.com>
2015-07-13 11:25:58 -0700

    N = 2515

--- src/regrf.c
+++ src/regrf.c
@@ -229,7 +229,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
       */
       
       
-      ran_multinomial(*sampsize, 2500, probs, coeffs);
+      ran_multinomial(*sampsize, 2515, probs, coeffs);
       
       /*
       coeffs[j] = coeffs;

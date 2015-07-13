28ce748cbb62703a65027d54de5a295a5af87687
Yenny Zhang <yennyszhang@gmail.com>
2015-07-13 11:49:34 -0700

    700, 3700

--- src/regrf.c
+++ src/regrf.c
@@ -229,7 +229,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
       */
       
       
-      ran_multinomial(*sampsize, 3680, probs, coeffs);
+      ran_multinomial(*sampsize, 3700, probs, coeffs);
       
       /*
       coeffs[j] = coeffs;

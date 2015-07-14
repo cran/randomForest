90986463c2a5633fc7e8e32dfec049c6c20f08e6
Yenny Zhang <yennyszhang@gmail.com>
2015-07-14 10:11:05 -0700

    why won't this output!?!

--- src/regrf.c
+++ src/regrf.c
@@ -202,7 +202,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
           */
       
       for (k = 0; k < *sampsize; k++) 
-    {
+      {
         probs[k] = 1.0 / *sampsize;
       }
       
@@ -226,7 +226,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
       */
       
       
-      ran_multinomial(*sampsize, *bigN, probs, coeffs);
+      /*ran_multinomial(*sampsize, *bigN, probs, coeffs);*/
 
       
       /*

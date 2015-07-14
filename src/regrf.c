6e4dac9cc2321ff1850533969f63c6b0139162e8
Yenny Zhang <yennyszhang@gmail.com>
2015-07-14 09:52:43 -0700

    attempting to put in bigN

--- src/regrf.c
+++ src/regrf.c
@@ -65,7 +65,7 @@ void ran_multinomial (int K, int N,
 
 void regRF(double *x, double *y, int *xdim, int *sampsize,
 	   int *nthsize, int *nrnodes, int *nTree, int *mtry, 
-     /*int *bigN,*/
+     int *bigN, 
      int *imp,
 	   int *cat, int *maxcat, int *jprint, int *doProx, int *oobprox,
            int *biasCorr, double *yptr, double *errimp, double *impmat,
@@ -226,7 +226,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
       */
       
       
-      ran_multinomial(*sampsize, 100000, probs, coeffs);
+      ran_multinomial(*sampsize, *bigN, probs, coeffs);
 
       
       /*

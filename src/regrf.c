e0c0bc6909f7a6e983f4a471a6c45b41bacbc459
Yenny Zhang <yennyszhang@gmail.com>
2015-07-15 16:56:24 -0700

    just checking

--- src/regrf.c
+++ src/regrf.c
@@ -203,7 +203,7 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
       
       for (k = 0; k < *sampsize; k++) 
       {
-        probs[k] = 1.0 / *sampsize;
+        probs[k] = 1.0 / nsample;
       }
       /*
       int fakecoeffs[*sampsize]; 

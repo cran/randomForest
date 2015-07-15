9b37e72feb7e59ba529b09a9df81a62e81e3ebff
Yenny Zhang <yennyszhang@gmail.com>
2015-07-15 16:58:10 -0700

    trying

--- src/regrf.c
+++ src/regrf.c
@@ -72,8 +72,9 @@ void regRF(double *x, double *y, int *xdim, int *sampsize,
            double *upper, double *mse, int *keepf, int *replace,
            int *testdat, double *xts, int *nts, double *yts, int *labelts,
            double *yTestPred, double *proxts, double *msets, double *coef,
+           double *probs,
            int *rainbow,
-           int *coeffs, double *probs, int *bigN, 
+           int *coeffs, int *bigN, 
            int *nout, int *inbag) {
     /*************************************************************************
    Input:
